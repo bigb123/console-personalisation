0db712253ba895ad5ee77f2ee2ca61af5132d23c
Piotr Pałka <piotr.palka123@gmail.com>
2018-05-29 10:54:03 +1000

    Added link to bash prompt documentation

--- personalisation.sh
+++ personalisation.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Colorful prompt with date on the beginning
+# Colorful prompt with date on the beginning. Explanation: https://wiki.archlinux.org/index.php/Bash/Prompt_customization
 PS1='\D{%d-%m-%y %k:%M} \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;36m\]\W\[\033[00m\]\$ '
